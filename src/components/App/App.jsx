54e03b4883926e3c41d5e6829268ca4fdee0beba
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-21 21:14:36 +0200

    build: routing logic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,37 @@
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+import ErrorPage from 'pages/404Page';
+import AddRecipePage from 'pages/AddRecipePage';
+import DrinksPage from 'pages/DrinksPage';
+import FavoritePage from 'pages/FavoritePage';
+import MainPage from 'pages/MainPage';
+import MyRecipesPage from 'pages/MyRecipesPage';
+import RecipePage from 'pages/RecipePage';
+import SigninPage from 'pages/SigninPage';
+import SignupPage from 'pages/SignupPage';
+import WelcomePage from 'pages/WelcomePage';
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
   return (
     <Routes>
+      <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/Signin" element={<SigninPage />} />
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="login" element={<RestrictedRoute component={Login} />} />
-        <Route
+        <Route index element={<MainPage />}></Route>
+        <Route path="/drinks/:categoryName" element={<DrinksPage />} />
+        <Route path="/add" element={<AddRecipePage />} />
+        <Route path="/my" element={<MyRecipesPage />} />
+        <Route path="/favorite" element={<FavoritePage />} />
+        <Route path="/recipe/:recipeId" element={<RecipePage />} />
+        {/* <Route
           path="register"
           element={<RestrictedRoute component={Register} />}
         />
         <Route
           path="contacts"
           element={<PrivateRoute component={Contacts} />}
-        />
+        /> */}
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,17 @@
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
+import { Outlet } from 'react-router-dom';
+
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default SharedLayout;
