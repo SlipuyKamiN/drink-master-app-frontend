7d81c171b973ab8d64a3f6300e38f1dab5e8a9e6
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-22 22:30:55 +0200

    fix: styles experiment

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,12 @@
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
-import base from 'styles/common/_base.module.scss';
 
 const SharedLayout = () => {
   return (
     <div>
       <Header />
-      <main className={base.container}>
+      <main>
         <Outlet />
       </main>
       <Footer />
