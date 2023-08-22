a303c6b8a59ae807220cdd4bff9bf542c50ad9b7
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-22 22:28:37 +0200

    build: basic project structure

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,13 @@
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import base from 'styles/common/_base.module.scss';
 
 const SharedLayout = () => {
   return (
     <div>
       <Header />
-      <main>
+      <main className={base.container}>
         <Outlet />
       </main>
       <Footer />
--- src/redux/recipesSlice.js
+++ src/redux/recipesSlice.js
@@ -8,27 +8,27 @@ export const recipesApi = createApi({
     baseUrl: BASE_URL,
   }),
   tagTypes: ['recipes'],
-  endpoints: builder => ({
-    fetchAll: builder.query({
-      query: () => ({ url: '/recipes' }),
-      providesTags: ['recipes'],
-    }),
-    addRecipe: builder.mutation({
-      query: ({ name, number }) => ({
-        url: '/recipes',
-        method: 'POST',
-        body: { name, number },
-      }),
-      invalidatesTags: ['recipes'],
-    }),
-    deleteRecipe: builder.mutation({
-      query: ({ id }) => ({
-        url: `/recipes/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['recipes'],
-    }),
-  }),
+  // endpoints: builder => ({
+  //   fetchAll: builder.query({
+  //     query: () => ({ url: '/recipes' }),
+  //     providesTags: ['recipes'],
+  //   }),
+  //   addRecipe: builder.mutation({
+  //     query: ({ name, number }) => ({
+  //       url: '/recipes',
+  //       method: 'POST',
+  //       body: { name, number },
+  //     }),
+  //     invalidatesTags: ['recipes'],
+  //   }),
+  //   deleteRecipe: builder.mutation({
+  //     query: ({ id }) => ({
+  //       url: `/recipes/${id}`,
+  //       method: 'DELETE',
+  //     }),
+  //     invalidatesTags: ['recipes'],
+  //   }),
+  // }),
 });
 
 export const {
