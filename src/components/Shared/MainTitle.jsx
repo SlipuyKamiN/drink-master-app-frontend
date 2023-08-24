5d17ed6a9b9587bc50111a8b53cdc3f1b87b65d8
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-08-24 17:29:11 +0300

    fix style component

--- src/components/Shared/MainTitle.jsx
+++ src/components/Shared/MainTitle.jsx
@@ -1,7 +1,11 @@
 import sass from './MainTitle.module.scss';
 
-const MainTitle = ({ title }) => {
-  return <h2 className={sass.title}>{title}</h2>;
+const MainTitle = ({ children, margin = '0', padding = '0' }) => {
+  return (
+    <h2 className={sass.title} style={{ margin, padding }}>
+      {children}
+    </h2>
+  );
 };
 
 export default MainTitle;
