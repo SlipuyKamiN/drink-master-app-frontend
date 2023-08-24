8edc0ae7de8f464942a6b0cf7947e90ee4e68013
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-08-24 22:07:42 +0300

    update style

--- src/components/Shared/MainTitle.jsx
+++ src/components/Shared/MainTitle.jsx
@@ -1,11 +1,7 @@
 import sass from './MainTitle.module.scss';
 
-const MainTitle = ({ children, margin = '0', padding = '0' }) => {
-  return (
-    <h2 className={sass.title} style={{ margin, padding }}>
-      {children}
-    </h2>
-  );
+const MainTitle = ({ title }) => {
+  return <h2 className={sass.title}>{title}</h2>;
 };
 
 export default MainTitle;
