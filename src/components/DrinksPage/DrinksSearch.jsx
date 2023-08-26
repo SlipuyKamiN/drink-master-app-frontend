70a38ca33a0b35999644365c3445e465664580ef
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-08-26 13:54:41 +0300

    added selects

--- src/components/DrinksPage/DrinksSearch.jsx
+++ src/components/DrinksPage/DrinksSearch.jsx
@@ -1,5 +1,36 @@
+// import cocktails from './cocktails.json';
+import categories from './categories.json';
+import ingredients from './ingredients.json';
+import Select from 'react-select';
+import sass from './DrinksSearch.module.scss';
+
+const categoriesOptions = categories.map(item => {
+  return { value: item, label: item };
+});
+
+const ingredientsOptions = ingredients.map(item => {
+  return { value: item.title, label: item.title };
+});
+
 const DrinksSearch = () => {
-  return <div>DrinksSearch</div>;
+  return (
+    <div>
+      <Select
+        className={sass.select}
+        classNamePrefix="select"
+        defaultValue={categoriesOptions[0]}
+        name="category"
+        options={categoriesOptions}
+      />
+      <Select
+        className={sass.select}
+        classNamePrefix="select"
+        defaultValue={ingredientsOptions[0]}
+        name="glasses"
+        options={ingredientsOptions}
+      />
+    </div>
+  );
 };
 
 export default DrinksSearch;
--- src/pages/DrinksPage.jsx
+++ src/pages/DrinksPage.jsx
@@ -1,5 +1,17 @@
+import DrinksSearch from 'components/DrinksPage/DrinksSearch';
+import DrinksList from 'components/DrinksPage/DrinksList';
+import Container from 'components/Shared/Container';
+import MainTitle from 'components/Shared/MainTitle';
+
 const DrinksPage = () => {
-  return <div>DrinksPage</div>;
+  return (
+    <Container>
+      <MainTitle title="Drinks"></MainTitle>
+      <DrinksSearch></DrinksSearch>
+      <DrinksList></DrinksList>
+      {/* <div>DrinksPage</div> */}
+    </Container>
+  );
 };
 
 export default DrinksPage;
