c01db3dc5b7b1fcaf29469b15b3dc4f65330a8ac
Viktor Pochynok <viktor83pochynok@gmail.com>
2023-08-27 13:18:48 +0300

    add thumb-placeholder

--- src/components/Shared/DrinkCard.jsx
+++ src/components/Shared/DrinkCard.jsx
@@ -12,7 +12,48 @@ const DrinkCard = ({ data }) => {
   return (
     <div className={sass.card}>
       <div className={sass.cardThumb}>
-        <img src={`${drinkThumb}`} alt={drink} />
+        {/* <img src={`${drinkThumb}`} alt={drink} /> */}
+        <picture>
+          <source
+            media="(min-width: 1440px)"
+            srcset={
+              drinkThumb
+                ? `${drinkThumb}`
+                : '../../images/thumb-placeholder-large.png'
+            }
+            type="image/png"
+          />
+
+          <source
+            media="(min-width: 768px)"
+            srcset={
+              drinkThumb
+                ? `${drinkThumb}`
+                : '../../images/thumb-placeholder-medium.png'
+            }
+            type="image/png"
+          />
+
+          <source
+            media="(max-width: 767.99px)"
+            srcset={
+              drinkThumb
+                ? `${drinkThumb}`
+                : '../../images/thumb-placeholder-small.png'
+            }
+            type="image/png"
+          />
+
+          <img
+            src={
+              drinkThumb
+                ? `${drinkThumb}`
+                : '../../images/thumb-placeholder-small.png'
+            }
+            alt={drink}
+            loading="lazy"
+          />
+        </picture>
       </div>
       <div className={sass.cardInfo}>
         <p className={sass.cardName}>{drink}</p>
