ead2201725b51cf674e14087eb66d65bcbf48ba4
Bad-Raider <stanislav.gusiev@gmail.com>
2023-08-27 21:55:45 +0300

    update pagination style
    added margin-bottom

--- src/components/FavoritePage/RecipesList.jsx
+++ src/components/FavoritePage/RecipesList.jsx
@@ -14,6 +14,10 @@ const RecipesList = () => {
     <RecipesItem />
     <RecipesItem />
     <RecipesItem />
+        <RecipesItem />
+    <RecipesItem />
+    <RecipesItem />
+
   </ul>
   </section>
 };
