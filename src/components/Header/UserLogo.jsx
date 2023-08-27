9bd58d7c49e3dc8a0cfc7e2cc005ac8fb9ec666e
alex-k22 <alexey.kovalov@gmail.com>
2023-08-27 22:12:56 +0000

    from linux

--- src/components/Header/UserLogo.jsx
+++ src/components/Header/UserLogo.jsx
@@ -1,9 +1,10 @@
-import css from "./UserLogo.module.scss";
+import css from './UserLogo.module.scss';
 
 const UserLogo = () => {
   return (
     <button className={css.btn} type="button">
-<p className={css.text}>{user.name}</p>
+      <img src="" alt=""User icon height="44px" width="44px"/>
+      <p className={css.text}>Alexey</p>
     </button>
   );
 };
