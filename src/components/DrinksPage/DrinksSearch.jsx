79d3263bd3f67cf6f67db11c83d2fa0d0b6b7b57
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-08-27 23:06:22 +0300

    changed some logic

--- src/components/DrinksPage/DrinksSearch.jsx
+++ src/components/DrinksPage/DrinksSearch.jsx
@@ -1,6 +1,3 @@
-// delete after backend connecting VVVVVVVVVV
-// import cocktails from './cocktails.json';
-// delete after backend connecting ^^^^^^^^^^^
 import { useCallback, useEffect } from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import categories from './categories.json';
@@ -24,10 +21,6 @@ const ingridientsOptions = ingridients.map(item => {
 
 const DrinksSearch = () => {
   const { register, handleSubmit } = useForm();
-  // const [searchedCocktail, setSearchedCocktail] = useState('');
-  // const [selectedCategory, setSelectedCategory] = useState('Cocktail');
-  // const [selectedIngredients, setSelectedIngredients] = useState('');
-  // const [perPage, setPerpage] = useState(10);
   const { width } = useWindowDimensions();
   const [searchParams, setSearchParams] = useSearchParams({
     category: 'Cocktail',
@@ -39,17 +32,7 @@ const DrinksSearch = () => {
     return Object.fromEntries([...searchParams]);
   }, [searchParams]);
 
-  // const search = searchParams.get('search');
-  // const ingredient = searchParams.get('ingredient');
-  // const category = searchParams.get('category');
-  // const page = searchParams.get('page');
-  // const limit = searchParams.get('limit');
-
-  // console.log(width);
-
   useEffect(() => {
-    // setPerpage(width >= 1440 ? 9 : 10);
-
     setSearchParams({
       ...getSearchParams(),
       limit: width >= 1440 ? 9 : 10,
@@ -60,59 +43,13 @@ const DrinksSearch = () => {
     console.log(getSearchParams());
   }, [search]);
 
-  // useEffect(() => {
-  // setSearchParams({
-  //   search: searchedCocktail,
-  //   ingredient: selectedIngredients,
-  //   category: selectedCategory,
-  //   page: 1,
-  //   limit: perPage,
-  // });
-
-  // delete after backend connecting VVVVVVVVVV
-
-  // const Params = {
-  //   searchedCocktail,
-  //   selectedCategory,
-  //   selectedIngredients,
-  //   perPage,
-  // };
-
-  // const filtred = cocktails.filter(
-  //   item =>
-  //     (!selectedCategory ||
-  //       item.category.toLowerCase() === selectedCategory.toLowerCase()) &&
-  //     (!selectedIngredients ||
-  //       item.ingredients.some(
-  //         subItem =>
-  //           subItem.title.toLowerCase() === selectedIngredients.toLowerCase()
-  //       )) &&
-  //     (!searchedCocktail ||
-  //       item.drink.toLowerCase().includes(searchedCocktail.toLowerCase()))
-  // );
-
-  // console.log(filtred);
-  // console.log(Params);
-
-  // delete after backend connecting ^^^^^^^^^^^
-  // }, [
-  //   searchParams,
-  //   setSearchParams,
-  //   perPage,
-  //   searchedCocktail,
-  //   selectedCategory,
-  //   selectedIngredients,
-  // ]);
-
   const handleSelectCategory = evt =>
-    // setSelectedCategory(evt.value === 'All categories' ? '' : evt.value);
     setSearchParams({
       ...getSearchParams(),
       category: evt.value === 'All categories' ? '' : evt.value,
     });
 
   const handleSelectIngridients = evt =>
-    // setSelectedIngredients(evt.value === 'All ingredients' ? '' : evt.value);
     setSearchParams({
       ...getSearchParams(),
       ingredient: evt.value === 'All ingridients' ? '' : evt.value,
@@ -149,7 +86,6 @@ const DrinksSearch = () => {
       <form
         className={sass.form}
         onSubmit={handleSubmit(data => {
-          // setSearchedCocktail(data.name);
           setSearchParams({
             ...getSearchParams(),
             search: data.name,
