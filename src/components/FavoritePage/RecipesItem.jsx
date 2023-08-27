9d9dd5e1e1b80b6edce4dafefe15f3f5193e0d1a
Bad-Raider <stanislav.gusiev@gmail.com>
2023-08-27 11:48:50 +0300

    recipe card
    create recipe card

--- src/components/FavoritePage/RecipesItem.jsx
+++ src/components/FavoritePage/RecipesItem.jsx
@@ -1,26 +1,41 @@
+ import { FiTrash2 } from 'react-icons/fi';
 import css from "./RecipesItem.module.scss";
-import placeholder from "../../images/no-content-cocktail.png";
-import resepies from '../../DB/cocktails.json';
+import placeholder from "../../images/thumb-placeholder-large.png";
 
-const useFetchAllQuery = () => resepies;
+// import resepies from '../../DB/cocktails.json';
+// const useFetchAllQuery = () => resepies;
 
 const RecipesItem = () => {
-  const recipies = useFetchAllQuery();
-  console.log(recipies);
+  // const recipies = useFetchAllQuery();
+  // console.log(recipies);
 
   
   return <>
     <li className={css.RecipesItem}>
       RecipesItem
       <div className={css.wraperCard}>
-        <img className={css.RecipesItemImg} src={placeholder} alt="glass" />
+        <img
+          className={css.RecipesItemImg}
+          src={placeholder}
+          alt="glass"
+        />
         <h3 className={css.RecipesItemTitle}>Title</h3>
         <h4 className={css.RecipesItemSubTitle}>ingridients</h4>
         <p className={css.RecipesItemText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio doloremque labore quaerat voluptatum laboriosam quam, itaque blanditiis dignissimos quasi, quia soluta pariatur quae totam sequi officia quis natus consequuntur? Architecto.</p>
 
         <div className={css.wraperBottom}>
-          <a className={css.RecipesItemLink} href="https://github.com/Bad-Raider/goit-react-hw-08-phonebook/tree/main/src">See recipe</a>
-          <button className={css.RecipesItemButton} type="button">button</button>
+          <a className={css.RecipesItemLink}
+            href="https://github.com/Bad-Raider/goit-react-hw-08-phonebook/tree/main/src">
+            See recipe
+          </a>
+          <button
+            className={css.RecipesItemButton}
+            type="button">
+            <FiTrash2
+              size={24}
+              color="#F3F3F3"
+            />
+          </button>
         </div>
       </div>
     </li>
