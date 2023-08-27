757d20a60f0f7fa7a4109563c93a4f416891ff3b
Viktor Pochynok <viktor83pochynok@gmail.com>
2023-08-27 15:50:49 +0300

    change hero

--- src/components/MainPage/AddYourCocktail.jsx
+++ src/components/MainPage/AddYourCocktail.jsx
@@ -7,15 +7,17 @@ import sass from './AddYourCocktail.module.scss';
 const AddYourCocktail = () => {
   return (
     <section className={sass.hero}>
-      <MainPageTitle name="Craft Your Perfect Drink with Drink Master"></MainPageTitle>
-      <p className={sass.content}>
-        Unlock your inner mixologist with Drink Master, your one-stop
-        destination for exploring, crafting, and mastering the world's finest
-        beverages.
-      </p>
-      <Link to="/add" className={sass.addPage}>
-        Add recipe
-      </Link>
+      <div className={sass.wrapper}>
+        <MainPageTitle name="Craft Your Perfect Drink with Drink Master"></MainPageTitle>
+        <p className={sass.content}>
+          Unlock your inner mixologist with Drink Master, your one-stop
+          destination for exploring, crafting, and mastering the world's finest
+          beverages.
+        </p>
+        <Link to="/add" className={sass.addPage}>
+          Add recipe
+        </Link>
+      </div>
     </section>
   );
 };
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,16 +2,12 @@ import Container from 'components/Shared/Container';
 import AddYourCocktail from 'components/MainPage/AddYourCocktail';
 // import PreviewDrinks from 'components/MainPage/PreviewDrinks';
 
-import sass from '../components/MainPage/MainPage.module.scss';
-
 const MainPage = () => {
   return (
-    <div className={sass.backgroundFoto}>
-      <Container>
-        <AddYourCocktail />
-        {/* <PreviewDrinks /> */}
-      </Container>
-    </div>
+    <>
+      <AddYourCocktail />
+      <Container>{/* <PreviewDrinks /> */}</Container>
+    </>
   );
 };
 
