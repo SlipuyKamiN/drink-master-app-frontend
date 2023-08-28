1d5527a96842ffb1fba98b01d406bf551b7910e2
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-08-28 22:09:40 +0300

    preupdate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,21 @@ import Logo from './Logo';
 import Navigation from './Navigation';
 import css from './Header.module.scss';
 import UserLogo from './UserLogo';
+import UserLogoModal from './UserLogoModal';
+import useWindowDimensions from 'hooks/useWindowDimensions';
+
 
 const Header = () => {
+  const {width} = useWindowDimensions();
+  
   return (
     <header className={css.header}>
       <Container>
         <div className={css.wrapper}>
           <Logo />
-          <Navigation />
+          {width > 1439 ? <Navigation /> : null}
           <UserLogo/>
+          <UserLogoModal/>
         </div>
       </Container>
     </header>
--- src/components/Header/UserLogoModal.jsx
+++ src/components/Header/UserLogoModal.jsx
@@ -1,5 +1,18 @@
+import css from './UserLogoModal.module.scss';
+import { FiEdit2 } from "react-icons/fi";
+
 const UserLogoModal = () => {
-  return <div>UserLogoModal</div>;
+  return (
+    <div className={css.modal}>
+       <button type="button" className={css.btn}>
+        <p>Edit profile</p>
+        <FiEdit2 height="14px" width="14px"/>
+      </button>
+      <button type="button" className={css.btn}>
+      Log out
+      </button>
+    </div>
+  );
 };
 
 export default UserLogoModal;
