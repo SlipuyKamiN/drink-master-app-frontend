3e891788b4ba5f78f297c0748450ee7ee982fbff
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-08-28 23:26:51 +0300

    add bg

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -4,10 +4,12 @@ import scss from './WelcomePage.module.scss';
 
 const WelcomePage = () => {
   return (
-    <div className={`${scss.container} ${scss.center}`}>
-      <AuthNavTitle title="Welcome to the app!" />
-      <AuthNav />
-    </div>
+    <section className={scss.wrapper}>
+      <div className={`${scss.container} ${scss.center}`}>
+        <AuthNavTitle title="Welcome to the app!" />
+        <AuthNav />
+      </div>
+    </section>
   );
 };
 
