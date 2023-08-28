5f1858b41d11fb32d5133fb415b7dc0c991877e9
TarasDer <tderev.switch@gmail.com>
2023-08-28 20:55:28 +0300

    added some styles

--- src/components/Footer/FollowUs.jsx
+++ src/components/Footer/FollowUs.jsx
@@ -1,42 +1,47 @@
-import styles from "./FollowUs.module.scss"
-import {BiLogoFacebook, BiLogoInstagram, BiLogoYoutube} from 'react-icons/bi'
-
+import styles from './FollowUs.module.scss';
+import {
+  BiLogoFacebook,
+  BiLogoInstagramAlt,
+  BiLogoYoutube,
+} from 'react-icons/bi';
 
 const FollowUs = () => {
-  return <div>
+  return (
+    <div>
       <ul className={styles.socialsList}>
-    <li className={styles.socialsItem}>
-                    <a
-                      className={styles.socialsLink}
-                      href="http://facebook.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <BiLogoFacebook className={styles.socialsIcon}/>
-                    </a>
-                  </li>
-                  <li className={styles.socialsItem}>
-                    <a
-                      className={styles.socialsLink}
-                      href="http://instagram.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <BiLogoInstagram className={styles.socialsIcon}/>
-                    </a>
-                  </li>
-                  <li className={styles.socialsItem}>
-                    <a
-                      className={styles.socialsLink}
-                      href="http://youtube.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <BiLogoYoutube className={styles.socialsIcon}/>
-                    </a>
-                  </li>
-    </ul>
-  </div>;
+        <li className={styles.socialsItem}>
+          <a
+            className={styles.socialsLink}
+            href="http://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoFacebook className={styles.socialsIcon} />
+          </a>
+        </li>
+        <li className={styles.socialsItem}>
+          <a
+            className={styles.socialsLink}
+            href="http://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoInstagramAlt className={styles.socialsIcon} />
+          </a>
+        </li>
+        <li className={styles.socialsItem}>
+          <a
+            className={styles.socialsLink}
+            href="http://youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoYoutube className={styles.socialsIcon} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default FollowUs;
