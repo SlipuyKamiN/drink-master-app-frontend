cd53c118729c7d6cbf5e3313a3d7b07f05961f31
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-30 14:31:58 +0200

    build: connect private/restricted logic with auth

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from 'components/App/App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/drink-master-app-frontend">
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/drink-master-app-frontend">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
