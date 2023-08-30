a1ad4b1c2131927104fa80c59d70b9fb3332c608
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-08-30 19:27:19 +0300

    Add a link to the video instruction

--- src/components/RecipePage/RecipePageHero.jsx
+++ src/components/RecipePage/RecipePageHero.jsx
@@ -3,8 +3,8 @@ import { useToggleFavoriteMutation } from 'redux/recipesSlice';
 import Container from 'components/Shared/Container';
 import placeholder from 'images/thumb-placeholder-large.png';
 import LoadingSpinner from 'components/Shared/LoadingSpinner';
-import sass from './RecipePageHero.module.scss';
 import titleStyles from 'components/Shared/MainTitle.module.scss';
+import sass from './RecipePageHero.module.scss';
 
 const {
   heroWrapper,
@@ -14,9 +14,11 @@ const {
   addToFavButton,
   beverageImg,
   beverageInfo,
+  videoLink,
 } = sass;
 
 const RecipePageHero = ({ recipe }) => {
+  console.log(recipe);
   const userId = useSelector(state => state.user._id);
 
   const isFavorite = recipe?.users.find(() => userId);
@@ -42,6 +44,16 @@ const RecipePageHero = ({ recipe }) => {
               : 'Add to favorite recipe'}
             {isLoading && <LoadingSpinner />}
           </button>
+          {recipe?.video && (
+            <a
+              href={recipe.video}
+              target="_blank"
+              rel="noreferrer"
+              className={videoLink}
+            >
+              Click to see a video instruction
+            </a>
+          )}
         </div>
 
         <img
