8ec3457b5a36babb5986761f8b873bdacdf60593
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-30 19:34:08 +0200

    build: hook form logic, + notification

--- src/components/Footer/validationSchema.js
+++ src/components/Footer/validationSchema.js
@@ -5,7 +5,7 @@ export const validationSchema = yup.object().shape({
     .string()
     .required('No email provided.')
     .matches(
-      '/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i',
+      '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$',
       'Email can contain letters, digits and may contain "@" and "." example@mail.com'
     ),
 });
