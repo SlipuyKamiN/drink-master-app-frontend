b0f77703a359e90727fe7637e730e2eb0657939b
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-08-30 20:06:22 +0300

    30.08.2023

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useSigninMutation } from 'redux/authSlice';
+
+
 const WelcomePage = () => {
+  const [dispatch] = useSigninMutation();
+  useEffect(()=> {
+    dispatch({ email: 'alex@mail.com', password: 'Passw0rd' });//
+  },[dispatch])
+
   return <div>WelcomePage</div>;
 };
 
