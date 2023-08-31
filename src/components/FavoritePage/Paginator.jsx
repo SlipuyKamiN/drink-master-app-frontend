287c8b6f5a073c145e9829006606e7fac2d96d5d
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-08-31 23:21:09 +0200

    made pagination

--- src/components/FavoritePage/Paginator.jsx
+++ src/components/FavoritePage/Paginator.jsx
@@ -1,38 +1,31 @@
-import css from "./Paginator.module.scss";
+import { useSearchParams } from 'react-router-dom';
+import css from './Paginator.module.scss';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+const Paginator = ({ pagesQty }) => {
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
 
-const Paginator = ({ totalHits,  setCurrentPage}) => {
-
-  console.log("totalHits:", totalHits);
-  let litit = 3;
-  const numberOfButtons = Math.ceil(totalHits / litit);
-  const numbersArray = Array.from({ length: numberOfButtons }, (_, index) => index + 1);
-
-  const handleBtnPaginations = (num) => {
-    setCurrentPage(num);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  const handleChangePage = page => {
+    setSearchParams({ page });
   };
 
+  const buttons = [];
 
+  for (let i = 0; i < pagesQty; i += 1) {
+    buttons.push(i + 1);
+  }
 
   return (
     <div className={css.PaginatorContainer}>
-      <button
-        className={ css.PaginatorBtn}
-        type="button"
-        name="PreviousBtn"
-      >
-        <FiChevronLeft
-          className={css.arrow}
-          size={27}
-        />
+      <button className={css.PaginatorBtn} type="button" name="PreviousBtn">
+        <FiChevronLeft className={css.arrow} size={27} />
       </button>
       <ul className={css.PaginatorList}>
-         {numbersArray.map((buttonNumber) => (
-           <button
-             className={css.PaginatorBtn}
-             onClick={()=>handleBtnPaginations(buttonNumber)}
+        {buttons.map(buttonNumber => (
+          <button
+            key={buttonNumber}
+            className={css.PaginatorBtn}
+            onClick={() => handleChangePage(buttonNumber)}
             name={buttonNumber}
             type="button"
           >
@@ -40,18 +33,11 @@ const Paginator = ({ totalHits,  setCurrentPage}) => {
           </button>
         ))}
       </ul>
-      <button
-        className={css.PaginatorBtn}
-        type="button"
-        name="NextBtn"
-      >
-        <FiChevronRight
-          className={css.arrow}
-          size={27}
-        />
+      <button className={css.PaginatorBtn} type="button" name="NextBtn">
+        <FiChevronRight className={css.arrow} size={27} />
       </button>
     </div>
-  )
+  );
 };
 
 export default Paginator;
--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,40 +1,55 @@
-import { useGetFavoritesQuery } from "redux/recipesSlice";
-import Paginator from "components/FavoritePage/Paginator";
-import RecipesList from "components/FavoritePage/RecipesList";
+import Paginator from 'components/FavoritePage/Paginator';
+import RecipesList from 'components/FavoritePage/RecipesList';
 import LoadingSpinner from 'components/Shared/LoadingSpinner';
-import { useState } from "react";
+import { notification } from 'components/Shared/notification';
+import { useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+import {
+  useGetFavoritesQuery,
+  useToggleFavoriteMutation,
+} from 'redux/recipesSlice';
 
 const FavoritePage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { data, isLoading, isError } = useGetFavoritesQuery(
+    `?page=${searchParams.get('page')}&limit=2`
+  );
+  const [toggleFavorite] = useToggleFavoriteMutation();
+  const pagesQty = Math.ceil(data?.totalHits / 2);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  
-  const { data, isLoading, isError } =
-    useGetFavoritesQuery(`?page=${currentPage}&limit=3`);
-  
+  useEffect(() => {
+    if (!searchParams.get('page')) {
+      setSearchParams({ page: 1 });
+      return;
+    }
+  }, [searchParams, setSearchParams]);
 
-  return <>
-    
-    {isLoading && <LoadingSpinner size={150} />}
-    {isError && <div>Ooooopsss!!!!</div>}
-    {data && <RecipesList data={data} />}
-    {data !== undefined && data.totalHits > 2 && data.totalHits !== undefined && <Paginator
-      totalHits={data.totalHits}
-      setCurrentPage={setCurrentPage}
-    />}
-    
-  </>
+  if (isLoading) return <LoadingSpinner />;
+
+  const removeFavorite = id => {
+    toggleFavorite(id)
+      .unwrap()
+      .then(() => {
+        if (pagesQty === 1) return;
+
+        if (data.favorites.length === 1) {
+          setSearchParams({ page: pagesQty - 1 });
+        }
+      });
+  };
+
+  return (
+    <>
+      {data?.totalHits && !isError ? (
+        <>
+          <RecipesList data={data} removeFavorite={removeFavorite} />
+          <Paginator pagesQty={pagesQty} />
+        </>
+      ) : (
+        <div>Your favorite list is empty</div>
+      )}
+    </>
+  );
 };
 
 export default FavoritePage;
-
-
-
-
-
-// import { useSigninMutation } from "redux/authSlice";
-// import { useEffect } from "react";
-
-  // const [dispatch, { data: userData,  }] = useSigninMutation();
-  // useEffect(() => {
-  //   dispatch({ email: "Stas@mail.com", password: "Ss123456" });
-  // }, [dispatch]);
