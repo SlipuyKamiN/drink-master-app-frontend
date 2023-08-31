61ef642aa837544a8fc86e8c25258f9cb03ce4a8
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-08-31 23:43:11 +0300

    31.08.2023

--- src/components/Header/UserInfoModal.jsx
+++ src/components/Header/UserInfoModal.jsx
@@ -1,5 +1,28 @@
+import { IoMdClose } from 'react-icons/io';
+import { useSelector } from 'react-redux';
+import css from './UserInfoModal.module.scss';
+import Modal from 'components/Shared/Modal';
+
 const UserInfoModal = () => {
-  return <div>UserInfoModal</div>;
+  const { name, avatarURL } = useSelector(({ user }) => user);
+  return (
+    <Modal>
+      <div  className={css.wrapper}>
+        <button type="button" className={css.closeBtn}>
+          <IoMdClose size={24} className={css.closeIcon} />
+        </button>
+        <img
+            src={avatarURL}
+            alt="User icon"
+            className={css.img}
+          />
+          <input 
+          value={name}
+          disabled="true"
+          ></input>
+      </div>
+    </Modal>
+  );
 };
 
 export default UserInfoModal;
--- src/components/Header/UserLogoModal.jsx
+++ src/components/Header/UserLogoModal.jsx
@@ -3,29 +3,45 @@ import { useState } from 'react';
 import { FiEdit2 } from 'react-icons/fi';
 import { useLogoutMutation } from 'redux/authSlice';
 import { notification } from 'components/Shared/notification';
+import UserInfoModal from './UserInfoModal';
+
 
 const UserLogoModal = () => {
   const [isDisabled, setIsDisabled] = useState(false);
+  const [showInfoModal, setShowInfoModal] = useState(false);
   const [dispatch] = useLogoutMutation();
-      
+
   const handleEditProfileClick = () => {
+    setShowInfoModal(!showInfoModal);
   };
 
   const handleSignoutClick = () => {
     setIsDisabled(true);
     dispatch();
-  }
+  };
 
- return (
-    <div className={css.modal}>
-      <button type="button" className={css.btn} onClick={handleEditProfileClick}>
-        <p className={css.text}>Edit profile</p>
-        <FiEdit2 height="14px" width="14px" />
-      </button>
-      <button type="button" className={`${css.btn} ${css.logoutBtn}`} onClick={handleSignoutClick} disabled={isDisabled}>
-        Log out
-      </button>
-    </div>
+  return (
+    <>
+      <div className={css.modal}>
+        <button
+          type="button"
+          className={css.btn}
+          onClick={handleEditProfileClick}
+        >
+          <p className={css.text}>Edit profile</p>
+          <FiEdit2 height="14px" width="14px" />
+        </button>
+        <button
+          type="button"
+          className={`${css.btn} ${css.logoutBtn}`}
+          onClick={handleSignoutClick}
+          disabled={isDisabled}
+        >
+          Log out
+        </button>
+      </div>
+      {showInfoModal && <UserInfoModal/>}
+    </>
   );
 };
 
