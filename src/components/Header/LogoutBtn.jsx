5be322936274a369f69fd4604e3afe2b77652af9
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-09-02 01:43:36 +0300

    02.09.2023

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { useLogoutMutation } from 'redux/authSlice';
+import css from './LogoutBtn.module.scss';
+import LoadingSpinner from 'components/Shared/LoadingSpinner';
+
 const LogoutBtn = () => {
-  return <div>LogoutBtn</div>;
+  const [dispatch, { isLoading }] = useLogoutMutation();
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleSignoutClick = () => {
+    setIsDisabled(true);
+    dispatch();
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className={css.btn}
+        onClick={handleSignoutClick}
+        disabled={isDisabled}
+      >
+        {isLoading ? <LoadingSpinner size={44} /> : 'Log out'}
+      </button>
+    </>
+  );
 };
 
 export default LogoutBtn;
--- src/components/Header/UserLogoModal.jsx
+++ src/components/Header/UserLogoModal.jsx
@@ -1,23 +1,15 @@
 import css from './UserLogoModal.module.scss';
 import { useState } from 'react';
 import { FiEdit2 } from 'react-icons/fi';
-import { useLogoutMutation } from 'redux/authSlice';
 import { notification } from 'components/Shared/notification';
 import UserInfoModal from './UserInfoModal';
-
+import LogoutBtn from './LogoutBtn';
 
 const UserLogoModal = () => {
-  const [isDisabled, setIsDisabled] = useState(false);
   const [showInfoModal, setShowInfoModal] = useState(false);
-  const [dispatch] = useLogoutMutation();
 
   const handleEditProfileClick = () => {
     setShowInfoModal(!showInfoModal);
-  };
-
-  const handleSignoutClick = () => {
-    setIsDisabled(true);
-    dispatch();
   };
 
   return (
@@ -31,16 +23,11 @@ const UserLogoModal = () => {
           <p className={css.text}>Edit profile</p>
           <FiEdit2 height="14px" width="14px" />
         </button>
-        <button
-          type="button"
-          className={`${css.btn} ${css.logoutBtn}`}
-          onClick={handleSignoutClick}
-          disabled={isDisabled}
-        >
-          Log out
-        </button>
+        <LogoutBtn />
       </div>
-      {showInfoModal && <UserInfoModal toggleModalShown={handleEditProfileClick}/>}
+      {showInfoModal && (
+        <UserInfoModal toggleModalShown={handleEditProfileClick} />
+      )}
     </>
   );
 };
