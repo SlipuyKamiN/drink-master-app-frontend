3abee74708fcc4a6fdef1f31d3449682be1e541f
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-09-02 16:42:35 +0300

    changed logic with wrong url

--- src/pages/DrinksPage.jsx
+++ src/pages/DrinksPage.jsx
@@ -2,27 +2,44 @@ import DrinksSearch from 'components/DrinksPage/DrinksSearch';
 import DrinksList from 'components/DrinksPage/DrinksList';
 import Container from 'components/Shared/Container';
 import DrinkPageTitle from 'components/DrinksPage/DrinkPageTitle';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import useWindowDimensions from '../hooks/useWindowDimensions';
 import { useCallback, useEffect, useState } from 'react';
 import { useSearchRecipesQuery } from '../redux/recipesSlice';
 import { useParams } from 'react-router-dom';
 import { notification } from 'components/Shared/notification';
+import { useGetCategoriesListQuery } from '../redux/recipesSlice';
 
 const DrinksPage = () => {
   const { categoryName: category } = useParams();
+  const { data: categoryList } = useGetCategoriesListQuery();
   const [query, setQuery] = useState('');
+  const [isFirstRender, setIsFirstRender] = useState(true);
   const { data, isError } = useSearchRecipesQuery(query);
   const { width } = useWindowDimensions();
   const [searchParams, setSearchParams] = useSearchParams({
-    category: category.replace('_', '/') || 'Cocktail',
+    category:
+      !category.replace('_', '/') || ''
+        ? 'Cocktail'
+        : category.replace('_', '/'),
     limit: 10,
     page: 1,
   });
   const getSearchParams = useCallback(() => {
     return Object.fromEntries([...searchParams]);
   }, [searchParams]);
-  const { search } = useLocation();
+
+  useEffect(() => {
+    if (categoryList && isFirstRender) {
+      if (!categoryList.includes(category)) {
+        setSearchParams({
+          ...getSearchParams(),
+          category: 'Cocktail',
+        });
+        setIsFirstRender(false);
+      }
+    }
+  }, [categoryList, isFirstRender, category, setSearchParams, getSearchParams]);
 
   useEffect(() => {
     setSearchParams({
@@ -69,7 +86,7 @@ const DrinksPage = () => {
     setQuery(
       `?search=${search}&category=${category}&ingredient=${ingredient}&limit=${limit}&page=${page}`
     );
-  }, [getSearchParams, search]);
+  }, [getSearchParams, isError]);
 
   if (isError) {
     notification('No drinks were found');
