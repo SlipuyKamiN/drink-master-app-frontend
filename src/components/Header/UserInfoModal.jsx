8042585c98c771897856904bba4e76426141fd9a
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-09-02 03:44:38 +0300

    02.09.2023

--- src/components/Header/UserInfoModal.jsx
+++ src/components/Header/UserInfoModal.jsx
@@ -4,12 +4,15 @@ import { IoMdClose } from 'react-icons/io';
 import { FiEdit2 } from 'react-icons/fi';
 import css from './UserInfoModal.module.scss';
 import Modal from 'components/Shared/Modal';
-import { ReactComponent as AddIcon } from "../../images/add photo.svg";
+import { ReactComponent as AddIcon } from '../../images/add photo.svg';
+import { setUser } from 'redux/userSlice';
 
 const UserInfoModal = ({ toggleModalShown }) => {
   const { name, avatarURL } = useSelector(({ user }) => user);
-  const [inputValue, setInputValue] = useState(name);
+  const [nameValue, setNameValue] = useState(name);
   const [nameIsDisabled, setNameIsDisabled] = useState(true);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [dispatch] = setUser();
   const inputField = useRef(null);
 
   const handleCloseModal = () => {
@@ -17,12 +20,18 @@ const UserInfoModal = ({ toggleModalShown }) => {
   };
 
   const handleNameChange = e => {
-    setInputValue(e.target.value);
+    setNameValue(e.target.value);
   };
 
-  const handleSaveChanges = async () => {
-    await setNameIsDisabled(false);
-    inputField.current.focus();
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    console.log(nameValue);
+    console.log(selectedImage);
+    const formData = new FormData();
+    formData.append('name', nameValue);
+    formData.append('avatarURL', selectedImage);
+    console.log(formData);
+    // dispatch(formData);
   };
 
   const handleEditName = async () => {
@@ -30,8 +39,8 @@ const UserInfoModal = ({ toggleModalShown }) => {
     inputField.current.focus();
   };
 
-  const handleAddIcon = () => {
-    console.log('yes');
+  const handleChooseIcon = event => {
+    setSelectedImage(event.target.files[0]);
   };
 
   return (
@@ -44,36 +53,50 @@ const UserInfoModal = ({ toggleModalShown }) => {
         >
           <IoMdClose size={24} className={css.closeIcon} />
         </button>
-        <div className={css.imgWrapper}>
-          <img src={avatarURL} alt="User icon" className={css.img} />
-          <button
-            type="button"
-            className={`${css.addImgBtn}`}
-            onClick={handleAddIcon}
-          >
-            <AddIcon size={28} className={css.editIcon} />
+        <form onSubmit={handleFormSubmit}>
+          <div className={css.imgWrapper}>
+            <img
+              src={
+                selectedImage ? URL.createObjectURL(selectedImage) : avatarURL
+              }
+              alt="User icon"
+              className={css.img}
+            />
+            <button
+              type="button"
+              className={`${css.addImgBtn}`}
+              onClick={handleChooseIcon}
+            >
+              <AddIcon size={28} className={css.editIcon} />
+            </button>
+            <input
+              name="name"
+              type="file"
+              accept="image/png, image/jpeg"
+              onChange={handleChooseIcon}
+            />
+          </div>
+          <div className={css.editWrapper}>
+            <input
+              className={css.input}
+              value={nameValue}
+              onChange={handleNameChange}
+              placeholder={name}
+              ref={inputField}
+              disabled={nameIsDisabled}
+            ></input>
+            <button
+              type="button"
+              className={`${css.closeBtn} ${css.editBtn}`}
+              onClick={handleEditName}
+            >
+              <FiEdit2 size={20} className={css.editIcon} />
+            </button>
+          </div>
+          <button type="submit" className={css.btn}>
+            Save changes
           </button>
-        </div>
-        <div className={css.editWrapper}>
-          <input
-            className={css.input}
-            value={inputValue}
-            onChange={handleNameChange}
-            placeholder={name}
-            ref={inputField}
-            disabled={nameIsDisabled}
-          ></input>
-          <button
-            type="button"
-            className={`${css.closeBtn} ${css.editBtn}`}
-            onClick={handleEditName}
-          >
-            <FiEdit2 size={20} className={css.editIcon} />
-          </button>
-        </div>
-        <button type="button" className={css.btn} onClick={handleSaveChanges}>
-          Save changes
-        </button>
+        </form>
       </div>
     </Modal>
   );
