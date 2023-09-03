73f6fa7c413e6a6f540295f3da93acb733408dd5
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-09-03 13:46:54 +0200

    fix: nav links

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,47 +1,32 @@
-import { NavLink } from "react-router-dom";
-import css from "./Navigation.module.scss";
+import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.scss';
 
 const Navigation = () => {
   return (
     <nav className={css.nav}>
       <ul className={css.list}>
         <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => `${isActive ? css.active : css.link}`}
-          >
+          <NavLink to="/" className={css.link}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/drinks"
-            className={({ isActive }) => `${isActive ? css.active : css.link}`}
-          >
+          <NavLink to="/drinks" className={css.link}>
             Drinks
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/add"
-            className={({ isActive }) => `${isActive ? css.active : css.link}`}
-          >
+          <NavLink to="/add" className={css.link}>
             Add recipe
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/my"
-            className={({ isActive }) => `${isActive ? css.active : css.link}`}
-          >
+          <NavLink to="/my" className={css.link}>
             My recipes
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/favorite"
-            className={({ isActive }) => `${isActive ? css.active : css.link}`}
-          >
+          <NavLink to="/favorite" className={css.link}>
             Favorites
           </NavLink>
         </li>
