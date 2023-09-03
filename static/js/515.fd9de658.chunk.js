645440c82ac77e616b6c8dd46f38acd0b91cd708
SlipuyKamiN <117014063+SlipuyKamiN@users.noreply.github.com>
2023-09-03 12:35:07 +0000

    Deploying to gh-pages from @ SlipuyKamiN/drink-master-app-frontend@e6c273bf1a6a44aa96edf2ef18db777c40b31fb5 🚀

--- static/js/515.fd9de658.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdrink_master_app_frontend=self.webpackChunkdrink_master_app_frontend||[]).push([[515],{7515:function(n,e,r){r.r(e);var t=r(184);e.default=function(){return(0,t.jsx)("div",{children:"DrinksPage"})}}}]);
-//# sourceMappingURL=515.fd9de658.chunk.js.map
