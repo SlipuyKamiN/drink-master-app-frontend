16f0a181df748289db4c108513893a6ca0329cab
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-09-04 21:16:34 +0300

    add btn clear img

--- src/components/AddRecipePage/AddResipeForm/ComponentForm/RecipeDescriptionFields.jsx
+++ src/components/AddRecipePage/AddResipeForm/ComponentForm/RecipeDescriptionFields.jsx
@@ -1,21 +1,25 @@
 import Select from 'react-select';
 import scss from './RecipeDescriptionFields.module.scss';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiX } from 'react-icons/fi';
 import {
   useGetCategoriesListQuery,
   useGetGlassListQuery,
 } from 'redux/recipesSlice';
 
-const RecipeDescriptionFields = ({ handleInputChange: {handleFileChange,
-  handleDrinkChange,
-  handleCategoryChange,
-  handleDescriptionChange,
-  handleGlassChange,}, value: {selectedImage, drink, description, category, glass, isShowError}}) => {
+const RecipeDescriptionFields = ({
+  handleInputChange: {
+    handleFileChange,
+    handleDrinkChange,
+    handleCategoryChange,
+    handleDescriptionChange,
+    handleGlassChange,
+  },
+  value: { selectedImage, drink, description, category, glass, isShowError },
+  clearImage,
+}) => {
   const { data: categoryList, isSuccess: isCategory } =
     useGetCategoriesListQuery('');
   const { data: glassList, isSuccess: isGlass } = useGetGlassListQuery('');
-
-  
 
   const getOptionsForSelect = listOptions => {
     const options = listOptions.map(item => {
@@ -28,13 +32,22 @@ const RecipeDescriptionFields = ({ handleInputChange: {handleFileChange,
     <div className={scss.form}>
       <div className={scss.form__wrapper}>
         {selectedImage ? (
-          <img
-            className={scss.img}
-            src={URL.createObjectURL(selectedImage)}
-            alt="Preview"
-          />
+          <div>
+            <img
+              className={scss.img}
+              src={URL.createObjectURL(selectedImage)}
+              alt="Preview"
+            />
+            <button
+              className={scss.img__clear}
+              onClick={clearImage}
+              type="button"
+            >
+              <FiX size="23" color="#f3f3f3" />
+            </button>
+          </div>
         ) : (
-            <div className={scss.file}>
+          <div className={scss.file}>
             <input
               className={scss.file__input}
               type="file"
@@ -46,9 +59,12 @@ const RecipeDescriptionFields = ({ handleInputChange: {handleFileChange,
               <FiPlus size="28" color="#161F37" />
             </button>
             <p className={scss.file__text}>Add image</p>
-            {isShowError && selectedImage === null && <p className={`${scss.error} ${scss.error__img}`}>The field image must be filled</p>}
-            </div>
-          
+            {isShowError && selectedImage === null && (
+              <p className={`${scss.error} ${scss.error__img}`}>
+                The field image must be filled
+              </p>
+            )}
+          </div>
         )}
       </div>
       <div className={scss.category}>
@@ -61,7 +77,11 @@ const RecipeDescriptionFields = ({ handleInputChange: {handleFileChange,
           onChange={handleDrinkChange}
           value={drink}
         />
-         {isShowError && drink === '' && <p className={`${scss.error} ${scss.error__drink}` }>The field drink must be filled</p>}
+        {isShowError && drink === '' && (
+          <p className={`${scss.error} ${scss.error__drink}`}>
+            The field drink must be filled
+          </p>
+        )}
         <input
           className={scss.form__input}
           type="text"
@@ -71,24 +91,36 @@ const RecipeDescriptionFields = ({ handleInputChange: {handleFileChange,
           value={description}
           placeholder="Enter about recipe"
         />
-       {isShowError && description === '' && <p className={`${scss.error} ${scss.error__description}` }>The field description must be filled</p>}
-          <Select
-            classNamePrefix="select-description"
-            placeholder=""
-            onChange={handleCategoryChange}
-            defaultValue={category}
-            options={isCategory ? getOptionsForSelect(categoryList) : []}
-          />
-        {isShowError && category === '' && <p className={`${scss.error} ${scss.error__category}` }>The field category must be filled</p>}
-        
-          <Select
-            classNamePrefix="select-description-glass"
-            placeholder=""
-            onChange={handleGlassChange}
-            defaultValue={glass}
-            options={isGlass ? getOptionsForSelect(glassList) : []}
-          />
-       {isShowError && glass === '' && <p className={`${scss.error} ${scss.error__glass}` }>The field glass must be filled</p>}
+        {isShowError && description === '' && (
+          <p className={`${scss.error} ${scss.error__description}`}>
+            The field description must be filled
+          </p>
+        )}
+        <Select
+          classNamePrefix="select-description"
+          placeholder=""
+          onChange={handleCategoryChange}
+          defaultValue={category}
+          options={isCategory ? getOptionsForSelect(categoryList) : []}
+        />
+        {isShowError && category === '' && (
+          <p className={`${scss.error} ${scss.error__category}`}>
+            The field category must be filled
+          </p>
+        )}
+
+        <Select
+          classNamePrefix="select-description-glass"
+          placeholder=""
+          onChange={handleGlassChange}
+          defaultValue={glass}
+          options={isGlass ? getOptionsForSelect(glassList) : []}
+        />
+        {isShowError && glass === '' && (
+          <p className={`${scss.error} ${scss.error__glass}`}>
+            The field glass must be filled
+          </p>
+        )}
       </div>
     </div>
   );
