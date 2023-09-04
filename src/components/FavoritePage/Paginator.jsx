4910f0d5c767be998692a69b88bf098091c2aff0
Bad-Raider <stanislav.gusiev@gmail.com>
2023-09-04 20:39:38 +0300

    update pagination style

--- src/components/FavoritePage/Paginator.jsx
+++ src/components/FavoritePage/Paginator.jsx
@@ -51,6 +51,7 @@ const Paginator = ({ pagesQty, params = {} }) => {
             }`}
             onClick={() => handleChangePage(buttonNumber)}
             name={buttonNumber}
+            disabled={buttons.length === 1}
             type="button"
           >
             {buttonNumber}
